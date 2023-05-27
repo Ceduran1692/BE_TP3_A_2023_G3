5683d11b9227c8c9c5f514518b3c191d468bad2c
FedeColombo <colombofederico17@gmail.com>
2023-05-26 21:34:40 -0300

    Pantalla de Login creada

--- app/src/main/java/ar/edu/ort/tpapp/ui/views/fragments/LoginFragment.kt
+++ app/src/main/java/ar/edu/ort/tpapp/ui/views/fragments/LoginFragment.kt
@@ -1,10 +1,18 @@
 package ar.edu.ort.tpapp.ui.views.fragments
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import ar.edu.ort.tpapp.R
 
 
@@ -18,4 +26,18 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+        /*btnLogin.setOnClickListener{
+            findNavController().navigate(action_loginFragment_to_homeScreenFragment)
+        }*/
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
+    }
 }
